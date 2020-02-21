377fbf726665a91646cbe4a29870b493a38b7849
Vitor Zortéa <vitorzortea@gmail.com>
2020-02-21 17:45:50 -0300

    feat: painel como estático

--- src/app/modules/painel/painel.module.ts
+++ src/app/modules/painel/painel.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { PainelRoutingModule } from './painel-routing.module';
 import { PainelComponent } from './painel.component';
 import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
-  declarations: [PainelComponent, MenuComponent],
+  declarations: [PainelComponent, MenuComponent, MainComponent],
   imports: [
     CommonModule,
     PainelRoutingModule
